bed4dee5a2f8b6c68e560cf923560d42544c9edf
SayedJafarHussainNaqvi <naqvish@rknec.edu>
2025-04-21 20:55:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami           = "ami-07a6f770277670015"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
+tags = {
+    Name = "MyFirstInstance"
+  }
 }
